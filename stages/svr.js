35a3958e71f6646639878254539568566d25d5a9
Jacek Kopecky <jacek@jacek.cz>
2021-02-22 09:39:37 +0000

    stage 2

--- stages/svr.js
+++ stages/svr.js
@@ -1,56 +1,18 @@
 // message board app
-// stage 8: Data belongs in a database
+// stage 2: add route for getting messages
 import express from 'express';
-import multer from 'multer';
-import * as mb from './messageboard.js';
 
 const app = express();
+app.use(express.static('client'));
 
-const uploader = multer({
-  dest: 'upload',
-  limits: { // for security
-    fields: 10,
-    fileSize: 1024 * 1024 * 20, // 20MB
-    files: 1,
-  },
-});
+const messages = [
+  'these are three default messages',
+  'delivered from the server',
+  'using a custom route',
+];
 
-app.use(express.static('client', { extensions: ['html'] }));
-
-async function getMessages(req, res) {
-  res.json(await mb.listMessages());
-}
-
-async function getMessage(req, res) {
-  const result = await mb.findMessage(req.params.id);
-  if (!result) {
-    res.status(404).send('No match for that ID.');
-    return;
-  }
-  res.json(result);
-}
-
-async function postMessage(req, res) {
-  const messages = await mb.addMessage(req.body.msg, req.file);
+app.get('/messages', (req, res) => {
   res.json(messages);
-}
-
-async function putMessage(req, res) {
-  const message = await mb.editMessage(req.body);
-  res.json(message);
-}
-
-// wrap async function for express.js error handling
-function asyncWrap(f) {
-  return (req, res, next) => {
-    Promise.resolve(f(req, res, next))
-      .catch((e) => next(e || new Error()));
-  };
-}
-
-app.get('/messages', asyncWrap(getMessages));
-app.get('/messages/:id', asyncWrap(getMessage));
-app.put('/messages/:id', express.json(), asyncWrap(putMessage));
-app.post('/messages', uploader.single('avatar'), express.json(), asyncWrap(postMessage));
+});
 
 app.listen(8080);
